0f46d2b4ed53aebceb5591abc290cdd5c63d1584
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-02-13 22:36:42 +0100

    simplify Application.run and Application.new_board

--- pathfinding-pygame/app.py
+++ pathfinding-pygame/app.py
@@ -1,4 +1,3 @@
-import sys
 import pygame as pg
 
 from utils import Dimension, Buttons
@@ -29,8 +28,7 @@ class Application:
         """
          Creates a new ``Application`` instance.
         """
-        pg.init()
-        pg.font.init()
+
         self.caption = "Basic Pathfinding"
         self.window = pg.display.set_mode((Dimension.SCREEN_WIDTH.value, Dimension.SCREEN_HEIGHT.value))
         self.board = Board(self.window)
@@ -42,9 +40,11 @@ class Application:
 
         :return: None
         """
+
         pg.display.set_caption(self.caption)
         self.board.draw_board()
-        while True:
+        running = True
+        while running:
 
             self.path_finder.draw_squares()
             if self.path_finder.path_able_to_find:
@@ -52,8 +52,7 @@ class Application:
                 self.path_finder.draw_path(path)
             for event in pg.event.get():
                 if event.type == pg.QUIT:
-                    pg.quit()
-                    sys.exit()
+                    running = False
 
                 if event.type == pg.MOUSEBUTTONUP and event.button == Buttons.MOUSE_LEFT.value:
                     self.choose_square()
@@ -61,17 +60,18 @@ class Application:
                 if event.type == pg.MOUSEBUTTONUP and event.button == Buttons.MOUSE_RIGHT.value:
                     self.clear_board()
 
-                if event.type == pg.KEYDOWN and event.key == pg.K_r:
+                if event.type == pg.KEYDOWN and event.key == Buttons.R_KEYBOARD.value:
                     self.new_board()
+
+        pg.quit()
 
     def new_board(self):
         """
         Create new obstacles and redrawn board.
         :return:
         """
-        self.path_finder.reset()
         self.board.recreate_obstacles()
-        self.board.draw_board()
+        self.clear_board()
 
     def choose_square(self):
         clicked_square_pos = Square.convert_to_board_coordinates(pg.mouse.get_pos())
