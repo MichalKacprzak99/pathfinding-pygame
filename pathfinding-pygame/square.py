9d00414e58c87a5dfe26776181faff3fee89c2cf
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-02-13 22:36:01 +0100

    improve comparator for Square objects

--- pathfinding-pygame/square.py
+++ pathfinding-pygame/square.py
@@ -51,7 +51,9 @@ class Square:
         pg.display.update(rect)
 
     def __eq__(self, other):
-        return (self.x, self.y) == (other.x, other.y)
+        if isinstance(other, Square):
+            return self.board_coordinates == other.board_coordinates
+        return NotImplemented
 
     def __hash__(self):
-        return hash((self.x, self.y))
+        return hash(self.board_coordinates)
